eb9f2ed1c3887efbe51d30921096737b74a3ccd9
Mike Martens <mam18@case.edu>
2019-10-29 11:45:51 -0400

    Initial commit

---
+++ Assets/CannonBarrel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBarrel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject earth = GameObject.Find("Earth2D");
+        Vector3 scaleTmp = this.transform.localScale;
+        Debug.Log(scaleTmp);
+        
+        scaleTmp.x /= earth.transform.localScale.x;
+        scaleTmp.y /= earth.transform.localScale.y;
+        scaleTmp.z /= earth.transform.localScale.z;
+        Debug.Log(scaleTmp);
+
+       // this.transform.localScale = scaleTmp;
+
+        this.transform.parent = earth.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Earth2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Earth2D : MonoBehaviour
+{
+    public float RevolutionPeriod;
+    private float previousTime;
+    Vector3 RotationAxis = new Vector3(0f, 1f, 0f);
+
+    void Start()
+    {
+        previousTime = Time.time;
+    }
+
+    void Update()
+    {
+        float angle = 360f * (Time.time - previousTime) / RevolutionPeriod;
+        previousTime = Time.time;
+        this.transform.Rotate(RotationAxis, angle);
+    }
+}
